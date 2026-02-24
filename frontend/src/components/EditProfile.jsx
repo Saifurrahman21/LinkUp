768865528aa94baa2bd6c60692040f9549aae1df
Saifurrahman <saifur13233@gmail.com>
2026-02-24 17:55:34 +0530

    Update edit form functionality

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -1,14 +1,100 @@
 import React from "react";
 import { RxCross1 } from "react-icons/rx";
+import { userDataContext } from "../context/userContext";
+import dp from "../assets/dp.webp";
+import { FiPlus } from "react-icons/fi";
+import { FiCamera } from "react-icons/fi";
 
-function EditProfile({ edit }) {
+function EditProfile() {
+  let { edit, setEdit, userData, setUserData } = useContext(userDataContext);
+  let [firstName, setFirstName] = useState(userData.firstName || "");
+  let [lastName, setLastName] = useState(userData.lastName || "");
+  let [userName, setUserName] = useState(userData.userName || "");
+  let [headline, setHeadline] = useState(userData.headline || "");
+  let [location, setLocation] = useState(userData.location || "");
+  let [gender, setGender] = useState(userData.gender || "");
+  let [skills, setSkills] = useState(userData.skills || []);
+  let [newSkill, setNewSkill] = useState([]);
+
   return (
     <div className="w-full h-[100vh] fixed top-0 z-[100] flex justify-center items-center">
       <div className="w-full h-full bg-black opacity-[0.5] absolute"></div>
-      <div className="w-[90%] max-w-[500px] h-[600px] bg-white relative z-[200] shadow-lg rounded-lg">
-        <div className="absolute top-[20px] right-[20px]">
-          <RxCross1 className="w-[25px] h-[25px] text-gray-800 font-bold" />
+      <div className="w-[90%] max-w-[500px] h-[600px] bg-white relative z-[200] shadow-lg rounded-lg p-[10px]">
+        <div
+          className="absolute top-[20px] right-[20px] cursor-pointer"
+          onClick={() => setEdit(false)}
+        >
+          <RxCross1 className="w-[25px] h-[25px] text-gray-800 font-bold cursor-pointer" />
         </div>
+        <div className=" w-full h-[150px] bg-gray-500 rounded-lg mt-[40px] overflow-hidden">
+          <img src="" alt="" className="w-full h-full" />
+          <FiCamera className="absolute right-[20px] top-[60px] w-[25px] h-[25px] text-white cursor-pointer" />
+          <div className="w-[80px] h-[80px] rounded-full overflow-hidden absolute top-[150px] ml-[20px]">
+            <img src={dp} alt="" className="w-full h-full" />
+          </div>
+          <div className="w-[10px] h-[10px] bg-[#17c1ff] absolute top-[200px] left-[90px] rounded-full flex justify-center items-center cursor-pointer">
+            <FiPlus className="text-white" />
+          </div>
+        </div>
+        <form
+          action=""
+          className=" w-full flex flex-col items-center justify-center gap-[20px] mt-[50px] "
+        >
+          <input
+            type="text"
+            placeholder="firstname"
+            className="w-full h-[50px] outline-none border-gray-600 px-[10px] py[5px] text-[18px] border-2 rounded-lg"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="lastname"
+            className="w-full h-[50px] outline-none border-gray-600 px-[10px] py[5px] text-[18px] border-2 rounded-lg"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="userName"
+            className="w-full h-[50px] outline-none border-gray-600 px-[10px] py[5px] text-[18px] border-2 rounded-lg"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="headline"
+            className="w-full h-[50px] outline-none border-gray-600 px-[10px] py[5px] text-[18px] border-2 rounded-lg"
+            value={headline}
+            onChange={(e) => setHeadline(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="location"
+            className="w-full h-[50px] outline-none border-gray-600 px-[10px] py[5px] text-[18px] border-2 rounded-lg"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="gender (male/female/other)"
+            className="w-full h-[50px] outline-none border-gray-600 px-[10px] py[5px] text-[18px] border-2 rounded-lg"
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+          />
+
+          <div>
+            <h1>Skills</h1>
+            {skills && (
+              <div>
+                {" "}
+                {skills.map((skill, index) => (
+                  <div key={index}>{skill}</div>
+                ))}
+              </div>
+            )}
+          </div>
+        </form>
       </div>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,19 +8,24 @@ import { userDataContext } from "../context/userContext";
 import EditProfile from "../components/EditProfile";
 
 function Home() {
-  let { userData, setUserData } = useContext(userDataContext);
+  let { userData, setUserData, edit, setEdit } = useContext(userDataContext);
 
-  let [edit, setEdit] = useState(false);
   return (
     <div className="w-full min-h-[100vh] bg-[#f0efe7] pt-[100px] flex items-start justify-center gap-[20px] px-[20px]">
-      <EditProfile edit={edit} />
+      {edit && <EditProfile />}
       <Nav />
       <div className="w-full lg:w-[25%] min-h-[200px] bg-white shadow-lg rounded-lg relative">
-        <div className="w-[100%] h-[100px] bg-gray-400 rounded overflow-hidden flex items-center justify-center cursor-pointer">
+        <div
+          className="w-[100%] h-[100px] bg-gray-400 rounded overflow-hidden flex items-center justify-center cursor-pointer"
+          onClick={() => setEdit(true)}
+        >
           <img src="" alt="" className="w-full" />
-          <FiCamera className="absolute right-[20px] top-[20px] w-[25px] h-[25px] text-gray-800 cursor-pointer" />
+          <FiCamera className="absolute right-[20px] top-[20px] w-[25px] h-[25px] text-white cursor-pointer" />
         </div>
-        <div className="w-[70px] h-[70px] rounded-full overflow-hidden items-center justify-center absolute top-[65px] left-[35px] cursor-pointer">
+        <div
+          className="w-[70px] h-[70px] rounded-full overflow-hidden items-center justify-center absolute top-[65px] left-[35px] cursor-pointer"
+          onClick={() => setEdit(true)}
+        >
           <img src={dp} alt="" className="h-full" />
         </div>
         <div className="w-[10px] h-[10px] bg-[#17c1ff] absolute top-[105px] left-[90px] rounded-full flex justify-center items-center cursor-pointer">
@@ -35,7 +40,10 @@ function Home() {
             {userData.location}
           </div>
         </div>
-        <button className="w-[100%] h-[40px] my-[20px] rounded-full border-2 border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]">
+        <button
+          className="w-[100%] h-[40px] my-[20px] rounded-full border-2 border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
+          onClick={() => setEdit(true)}
+        >
           Edit Profile <HiPencil />
         </button>
       </div>
