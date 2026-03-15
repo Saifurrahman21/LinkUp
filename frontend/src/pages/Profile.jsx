586129c596a70e1c4180d7f4905960f760980d4e
Saifurrahman <saifur13233@gmail.com>
2026-03-15 07:07:23 +0530

    Implement skills, experience, and education sections and complete profile page functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -7,9 +7,13 @@ import { HiPencil } from "react-icons/hi";
 import { userDataContext } from "../context/userContext";
 import EditProfile from "../components/EditProfile";
 import { authDataContext } from "../context/AuthContext";
+import Post from "../components/Post";
 
 function Profile() {
-  let { userData, setuserData, edit, setEdit } = useContext(userDataContext);
+  let { userData, setuserData, edit, setEdit, postData, setPostData } =
+    useContext(userDataContext);
+
+  let [profilePost, setProfilePost] = useState("");
   let { serverUrl } = useContext(authDataContext);
 
   let [userConnection, setUserConnection] = useState([]);
@@ -28,11 +32,15 @@ function Profile() {
     handleGetUserConnection();
   }, []);
 
+  useEffect(() => {
+    setProfilePost(postData.filter((post) => post.author._id == userData._id));
+  }, []);
+
   return (
-    <div className="w-full min-h-[100vh] bg-[#f0efe7] flex flex-col items-center pt-[100px]">
+    <div className="w-full min-h-[100vh] bg-[#f0efe7] flex flex-col items-center pt-[100px] pb-[40px]">
       <Nav />
       {edit && <EditProfile />}
-      <div className="w-full max-h-[900px] min-h-[100vh]">
+      <div className="w-full max-h-[900px] min-h-[100vh] flex flex-col gap-[10px]">
         <div className="relative bg-[white] pb-[40px] rounded shadow-lg">
           <div
             className="w-[100%] h-[100px] bg-gray-400 rounded overflow-hidden flex items-center justify-center cursor-pointer"
@@ -69,6 +77,82 @@ function Profile() {
             Edit Profile <HiPencil />
           </button>
         </div>
+
+        <div className="w-full min-h-[100px] flex items-center p-[40px] text-[22px] text-gray-600 font-semibold bg-white shadow-lg rounded-lg ">
+          {`Post (${profilePost.length})`}
+        </div>
+        {profilePost.map((post, index) => (
+          <Post
+            key={index}
+            id={post._id}
+            description={post.description}
+            author={post.author}
+            image={post.image}
+            like={post.like}
+            comment={post.comment}
+            createdAt={post.createdAt}
+          />
+        ))}
+        {userData.skills.length > 0 && (
+          <div className="w-full min-h-[100px] flex flex-col gap-[10px] justify-center items-center p-[20px] font-semibold bg-white shadow-lg rounded-lg ">
+            <div className="text-[22px] text-gray-600 ">Skills</div>
+            <div className="flex flex-wrap justify-start items-center gap-[20px] text-gray-700 p-[20px]">
+              {userData.skills.map((skill) => (
+                <div className="text-[20px]">{skill}</div>
+              ))}
+              <button
+                className="min-w-[150px] h-[40px]  rounded-full border-2 ml-[20px] border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
+                onClick={() => setEdit(true)}
+              >
+                Add Skills
+              </button>
+            </div>
+          </div>
+        )}
+        {userData.education.length > 0 && (
+          <div className="w-full min-h-[100px] flex flex-col gap-[10px] justify-center items-center p-[20px] font-semibold bg-white shadow-lg rounded-lg ">
+            <div className="text-[22px] text-gray-600 ">Education</div>
+            <div className="flex flex-col justify-start items-start gap-[20px] text-gray-700 p-[20px]">
+              {userData.education.map((edu) => (
+                <>
+                  <div className="text-[20px]">College : {edu.college}</div>
+                  <div className="text-[20px]">Degree : {edu.degree}</div>
+                  <div className="text-[20px]">
+                    Field Of Study : {edu.fieldOfStudy}
+                  </div>
+                </>
+              ))}
+              <button
+                className="min-w-[150px] h-[40px]  rounded-full border-2  border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
+                onClick={() => setEdit(true)}
+              >
+                Add Education
+              </button>
+            </div>
+          </div>
+        )}
+        {userData.experience.length > 0 && (
+          <div className="w-full min-h-[100px] flex flex-col gap-[10px] justify-center items-center p-[20px] font-semibold bg-white shadow-lg rounded-lg ">
+            <div className="text-[22px] text-gray-600 ">Experience</div>
+            <div className="flex flex-col justify-start items-start gap-[20px] text-gray-700 p-[20px]">
+              {userData.experience.map((ex) => (
+                <>
+                  <div className="text-[20px]">Title : {ex.title}</div>
+                  <div className="text-[20px]">Company : {ex.company}</div>
+                  <div className="text-[20px]">
+                    Description : {ex.description}
+                  </div>
+                </>
+              ))}
+              <button
+                className="min-w-[150px] h-[40px]  rounded-full border-2  border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
+                onClick={() => setEdit(true)}
+              >
+                Add Experience
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
