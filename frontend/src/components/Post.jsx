c2fc43f3ecaee97e854aa013d3c266ac9acd61db
Saifurrahman <saifur13233@gmail.com>
2026-03-18 16:39:07 +0530

    Enable case-insensitive search for names and skills to retrieve matching profiles

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -16,7 +16,8 @@ let socket = io("http://localhost:8000");
 function Post({ id, author, like, comment, description, image, createdAt }) {
   let [more, setMore] = useState(false);
   let { serverUrl } = useContext(authDataContext);
-  let [userData, setuserData, getPost] = useContext(userDataContext);
+  let [userData, setuserData, getPost, handleGetProfile] =
+    useContext(userDataContext);
   let [likes, setLikes] = useState(like || []);
   let [commentsContent, setCommentsContent] = useState("");
   let [comments, setComments] = useState(comment || []);
@@ -76,7 +77,10 @@ function Post({ id, author, like, comment, description, image, createdAt }) {
   return (
     <div className="w-full min-h-[200px] flex flex-col gap-[10px] bg-white rounded-lg shadow-lg p-[20px]">
       <div className="flex justify-between items-center">
-        <div className="flex justify-center items-start gap-[10px]">
+        <div
+          className="flex justify-center items-start gap-[10px]"
+          onClick={() => handleGetProfile(author.userName)}
+        >
           <div className="w-[70px] h-[70px] rounded-full overflow-hidden flex items-center justify-center  cursor-pointer">
             <img src={author.profileImage || dp} alt="" className="h-full" />
           </div>
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -8,32 +8,27 @@ import { userDataContext } from "../context/userContext";
 import EditProfile from "../components/EditProfile";
 import { authDataContext } from "../context/AuthContext";
 import Post from "../components/Post";
+import ConnectionButton from "../components/ConnectionButton";
 
 function Profile() {
-  let { userData, setuserData, edit, setEdit, postData, setPostData } =
-    useContext(userDataContext);
+  let {
+    userData,
+    setuserData,
+    edit,
+    setEdit,
+    postData,
+    setPostData,
+    profileData,
+    setProfileData,
+  } = useContext(userDataContext);
 
   let [profilePost, setProfilePost] = useState("");
   let { serverUrl } = useContext(authDataContext);
 
-  let [userConnection, setUserConnection] = useState([]);
-
-  const handleGetUserConnection = async () => {
-    try {
-      let result = await axios.get(`${serverUrl}/api/connection`, {
-        withCredentials: true,
-      });
-      setUserConnection(result.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   useEffect(() => {
-    handleGetUserConnection();
-  }, []);
-
-  useEffect(() => {
-    setProfilePost(postData.filter((post) => post.author._id == userData._id));
+    setProfilePost(
+      postData.filter((post) => post.author._id == profileData._id),
+    );
   }, []);
 
   return (
@@ -46,36 +41,49 @@ function Profile() {
             className="w-[100%] h-[100px] bg-gray-400 rounded overflow-hidden flex items-center justify-center cursor-pointer"
             onClick={() => setEdit(true)}
           >
-            <img src={userData.coverImage || ""} alt="" className="w-full" />
+            <img src={profileData.coverImage || ""} alt="" className="w-full" />
             <FiCamera className="absolute right-[20px] top-[20px] w-[25px] h-[25px] text-white cursor-pointer" />
           </div>
           <div
             className="w-[70px] h-[70px] rounded-full overflow-hidden flex items-center justify-center absolute top-[65px] left-[35px] cursor-pointer"
             onClick={() => setEdit(true)}
           >
-            <img src={userData.profileImage || dp} alt="" className="h-full" />
+            <img
+              src={profileData.profileImage || dp}
+              alt=""
+              className="h-full"
+            />
           </div>
           <div className="w-[10px] h-[10px] bg-[#17c1ff] absolute top-[105px] left-[90px] rounded-full flex justify-center items-center cursor-pointer">
             <FiPlus className="text-white" />
           </div>
           <div className="mt-[30px] pl-[20px]  font-semibold text-gray-700">
-            <div className="text-[22px]">{`${userData.firstName} ${userData.lastName}`}</div>
+            <div className="text-[22px]">{`${profileData.firstName} ${profileData.lastName}`}</div>
             <div className="text-[18px] font-semibold text-gray-600">
-              {userData.headline || ""}
+              {profileData.headline || ""}
             </div>
             <div className="text-[16px] font-semibold text-gray-500">
-              {userData.location}
+              {profileData.location}
             </div>
             <div className="text-[16px] font-semibold text-gray-500">
-              {userConnection.length}
+              {profileData.connection.length}
             </div>
           </div>
-          <button
-            className="min-w-[150px] h-[40px] my-[20px] rounded-full border-2 ml-[20px] border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
-            onClick={() => setEdit(true)}
-          >
-            Edit Profile <HiPencil />
-          </button>
+
+          {profileData._id == userData._id && (
+            <button
+              className="min-w-[150px] h-[40px] my-[20px] rounded-full border-2 ml-[20px] border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
+              onClick={() => setEdit(true)}
+            >
+              Edit Profile <HiPencil />
+            </button>
+          )}
+
+          {profileData._id != userData._id && (
+            <div className="ml-[20px] mt-[20px]">
+              <ConnectionButton userId={profileData._id} />
+            </div>
+          )}
         </div>
 
         <div className="w-full min-h-[100px] flex items-center p-[40px] text-[22px] text-gray-600 font-semibold bg-white shadow-lg rounded-lg ">
@@ -93,27 +101,30 @@ function Profile() {
             createdAt={post.createdAt}
           />
         ))}
-        {userData.skills.length > 0 && (
+        {profileData.skills.length > 0 && (
           <div className="w-full min-h-[100px] flex flex-col gap-[10px] justify-center items-center p-[20px] font-semibold bg-white shadow-lg rounded-lg ">
             <div className="text-[22px] text-gray-600 ">Skills</div>
             <div className="flex flex-wrap justify-start items-center gap-[20px] text-gray-700 p-[20px]">
-              {userData.skills.map((skill) => (
+              {profileData.skills.map((skill) => (
                 <div className="text-[20px]">{skill}</div>
               ))}
-              <button
-                className="min-w-[150px] h-[40px]  rounded-full border-2 ml-[20px] border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
-                onClick={() => setEdit(true)}
-              >
-                Add Skills
-              </button>
+
+              {profileData._id == userData._id && (
+                <button
+                  className="min-w-[150px] h-[40px]  rounded-full border-2 ml-[20px] border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
+                  onClick={() => setEdit(true)}
+                >
+                  Add Skills
+                </button>
+              )}
             </div>
           </div>
         )}
-        {userData.education.length > 0 && (
+        {profileData.education.length > 0 && (
           <div className="w-full min-h-[100px] flex flex-col gap-[10px] justify-center items-center p-[20px] font-semibold bg-white shadow-lg rounded-lg ">
             <div className="text-[22px] text-gray-600 ">Education</div>
             <div className="flex flex-col justify-start items-start gap-[20px] text-gray-700 p-[20px]">
-              {userData.education.map((edu) => (
+              {profileData.education.map((edu) => (
                 <>
                   <div className="text-[20px]">College : {edu.college}</div>
                   <div className="text-[20px]">Degree : {edu.degree}</div>
@@ -122,20 +133,23 @@ function Profile() {
                   </div>
                 </>
               ))}
-              <button
-                className="min-w-[150px] h-[40px]  rounded-full border-2  border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
-                onClick={() => setEdit(true)}
-              >
-                Add Education
-              </button>
+
+              {profileData._id == userData._id && (
+                <button
+                  className="min-w-[150px] h-[40px]  rounded-full border-2  border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
+                  onClick={() => setEdit(true)}
+                >
+                  Add Education
+                </button>
+              )}
             </div>
           </div>
         )}
-        {userData.experience.length > 0 && (
+        {profileData.experience.length > 0 && (
           <div className="w-full min-h-[100px] flex flex-col gap-[10px] justify-center items-center p-[20px] font-semibold bg-white shadow-lg rounded-lg ">
             <div className="text-[22px] text-gray-600 ">Experience</div>
             <div className="flex flex-col justify-start items-start gap-[20px] text-gray-700 p-[20px]">
-              {userData.experience.map((ex) => (
+              {profileData.experience.map((ex) => (
                 <>
                   <div className="text-[20px]">Title : {ex.title}</div>
                   <div className="text-[20px]">Company : {ex.company}</div>
@@ -144,12 +158,15 @@ function Profile() {
                   </div>
                 </>
               ))}
-              <button
-                className="min-w-[150px] h-[40px]  rounded-full border-2  border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
-                onClick={() => setEdit(true)}
-              >
-                Add Experience
-              </button>
+
+              {profileData._id == userData._id && (
+                <button
+                  className="min-w-[150px] h-[40px]  rounded-full border-2  border-[#2dc0ff] text-[#2dc0ff] flex items-center justify-center gap-[10px]"
+                  onClick={() => setEdit(true)}
+                >
+                  Add Experience
+                </button>
+              )}
             </div>
           </div>
         )}
